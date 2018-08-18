75b4ccea839cfd1298c24b6e5d70b8bfbf9da3ec
Corrine Morita <corrine@promptt.com>
2018-08-18 11:46:51 -0700

    add links to App and pages for Dashboard Page

--- src/components/pages/DashboardPage.jsx
+++ src/components/pages/DashboardPage.jsx
@@ -1,7 +1,61 @@
-// this page shows how much was given over time for each kid
+import React, {Component} from 'react'
+import { Route, Link } from 'react-router-dom'
+import { Card, Icon, Image } from 'semantic-ui-react'
+// import {
+//   Button,
+//   Container,
+//   Divider,
+//   Grid,
+//   Header,
+//   Icon,
+//   Image,
+//   List,
+//   Menu,
+//   Responsive,
+//   Segment,
+//   Sidebar,
+//   Visibility,
+// } from 'semantic-ui-react'
 
+class DashboardPage extends Component {
 
+render () {
+  return (
 
+    <div>
+    {/* NAVIGATION at top */}
+    {/* <Segment
+      inverted
+      textAlign='center'
+      style={{ minHeight: 30, padding: '1em 0em', backgroundColor: "green" }}
+      vertical
+      >
+      <Menu
+      fixed={fixed ? 'top' : null}
+      inverted={!fixed}
+      pointing={!fixed}
+      secondary={!fixed}
+      size='large'
+      >
+      <Container style={{margin: 0}}>
+      <Menu.Item as={Link} to='/'>Home</Menu.Item>
+      <Menu.Item as='a'>Why Give?</Menu.Item>
+      <Menu.Item as='a' active>Charities</Menu.Item>
+      <Menu.Item as='a'>Donate Dashboard</Menu.Item>
+      </Container>
+      </Menu>
+      </Segment>  */}
+
+      <h3 style={{marginTop: '40px'}}>You have reached the Dashboard Page</h3>
+
+      </div>
+
+    )
+ }
+//component close bracket
+}
+
+export default DashboardPage
 
 //this is the fetch to collect ALL from PLEDGE route from 1 user
   // getPledgeInfo = async () => {
