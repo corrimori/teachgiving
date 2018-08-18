d45fd687c73766f671c2c6c5bb65e0b3207d0b47
Corrine Morita <corrine@promptt.com>
2018-08-18 11:25:24 -0700

    update copy on landpage

--- src/components/pages/LandPage.jsx
+++ src/components/pages/LandPage.jsx
@@ -213,7 +213,10 @@ const LandPage = () => (
           Encourage Generosity and Teach Giving
         </Header>
         <p style={{ fontSize: '1.33em' }}>
-          Giving is more satisfying that receiving... Teaching kids how good it feels to give at an early age blooms with possiblity of potential exponental growth of good for the future, their future. One of the biggest barriers to giving is that we don't have extra money in our budget. Nano-donations allow every kids to support something they care about. A little bit can frequently add up to a lot with dedication and consistency.
+          Giving is more satisfying that receiving... Teaching kids how good it feels to give at an early age blooms with possiblity of potential exponental growth of good for the future, their future.
+        </p>
+        <p style={{ fontSize: '1.33em' }}>
+          One of the biggest barriers to giving is that we typically don't have extra money in our budget. Nano-donations allow every kids to support something they care about. With dedication and consistency, a little bit can frequently add up to a lot a bit.
         </p>
         <Button as={Link} to='/charities' size='large'>
           Donate Today
